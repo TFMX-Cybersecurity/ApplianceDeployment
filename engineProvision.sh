5a2dc1b5098cd62cccecf754034cd441bef12375
Jason Mull <4792624+jasonmull@users.noreply.github.com>
2024-03-21 09:29:17 -0500

    Create engineProvision.sh

---
+++ engineProvision.sh
@@ -0,0 +1,5 @@
+#Install Docker
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update
+apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
